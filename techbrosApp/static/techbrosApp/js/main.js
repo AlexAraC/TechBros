d11974fee00b63ed2e4ec2e38a99707997ff9055
AlexAraC <arayaalex886@gmail.com>
2026-01-27 15:18:02 -0600

    Mejora hero con animaciones y logo

---
+++ techbrosApp/static/techbrosApp/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    VANTA.BIRDS({
+        el: "#hero-bg",
+        backgroundColor: 0x63291f,
+        backgroundAlpha: 1,
+        color1: 0xff6a00,
+        color2: 0xffb347,
+        quantity: 4,
+        birdSize: 1,
+        wingSpan: 30,
+        speedLimit: 5,
+        separation: 20,
+        alignment: 20,
+        cohesion: 20
+    });
+});
